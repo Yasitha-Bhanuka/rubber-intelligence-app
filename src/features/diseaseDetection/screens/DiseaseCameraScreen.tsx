554aa20c40ad20f7e1043a6c601c1af9a142d647
Yasitha-Dev <yasithabhanukac@gmail.com>
2025-12-31 02:27:49 +0530

    Disease Detection system partially done

--- src/features/diseaseDetection/screens/DiseaseCameraScreen.tsx
+++ src/features/diseaseDetection/screens/DiseaseCameraScreen.tsx
@@ -40,11 +40,12 @@ export const DiseaseCameraScreen = ({ navigation, route }: any) => {
 
     const pickImage = async () => {
         const result = await ImagePicker.launchImageLibraryAsync({
-            // @ts-ignore
-            mediaTypes: ImagePicker.MediaType.Images,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
-            quality: 1,
+            quality: 0.5,
+            selectionLimit: 1,
+            exif: false,
         });
 
         if (!result.canceled) {
