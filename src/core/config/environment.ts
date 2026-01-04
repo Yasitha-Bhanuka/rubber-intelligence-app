ec1a48c27b22093cf4f74bdfefdb434b1ded82cb
Yasitha-Dev <yasithabhanukac@gmail.com>
2026-01-04 15:28:25 +0530

    project is running well

--- src/core/config/environment.ts
+++ src/core/config/environment.ts
@@ -3,6 +3,6 @@ export const ENV = {
     // Use localhost for iOS Simulator
     // Use 10.0.2.2 for Android Emulator to access localhost
     // Use localhost for iOS Simulator
-    API_URL: process.env.EXPO_PUBLIC_API_URL || 'http://192.168.43.253:5001/api',
+    API_URL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5001/api',
     IS_DEV: __DEV__,
 };
